1dad51095539f6effb34e404d00ab61a79ea1ed5
zeynepakcil <zeynep.akcil@ug.bilkent.edu.tr>
2023-08-04 18:11:05 +0300

    NewGameButtonControl is included

---
+++ GUIHangman/NewGameButtonControl.java
@@ -0,0 +1,25 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class NewGameButtonControl extends JButton implements IHangmanView{
+    private Hangman hangman;
+
+    public NewGameButtonControl(HangmanModel hangman){
+        super("New Game");
+        this.hangman = hangman;
+        this.setEnabled(false);
+        this.addActionListener(e ->{
+                hangman.initNewGame();
+                this.setEnabled(false);
+            });
+                
+    }
+
+    @Override
+    public void updateView(Hangman hangmanModel) {
+        if(hangman.isGameOver())
+            setEnabled(true);
+    }
+}
