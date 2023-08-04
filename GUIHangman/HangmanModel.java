ac33e449a62fecdbf8f30d51ee752f3a570f716b
zeynepakcil <zeynep.akcil@ug.bilkent.edu.tr>
2023-08-04 18:08:10 +0300

    Hangman files are imported

---
+++ GUIHangman/HangmanModel.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+// HangmanModel - extends hangman adding support for single/multiple views & controllers
+// David, 2012/4/8
+
+public class HangmanModel extends Hangman{
+	ArrayList<IHangmanView> views;
+	// IHangmanView	view;
+
+	public HangmanModel( IHangmanSetup setup){
+		super( setup);
+		views = new ArrayList<IHangmanView>();
+		// view = null;
+	}
+
+	@Override
+	public int tryThis( char letter){
+		int result = super.tryThis( letter);
+		notifyViews();
+		return result;
+	}
+
+	@Override
+	public void initNewGame(){
+		super.initNewGame();
+		notifyViews();
+	}
+
+
+	// **********************************************************
+	// * Single View Support
+	// **********************************************************
+//	public void addView( IHangmanView view)
+//	{
+//		this.view = view;
+//		notifyViews();
+//	}
+//
+//	public void notifyViews()
+//	{
+//		if ( view != null)
+//			view.updateView( this);
+//	}
+
+
+	// **********************************************************
+	// * Multiple View Support
+	// **********************************************************
+	public void addView( IHangmanView view){
+		views.add( view);
+		notifyViews();
+	}
+
+	public void notifyViews(){
+		if ( views != null)
+			for ( IHangmanView view : views)
+				view.updateView( this);
+	}
+
+	public void removeView( IHangmanView view){
+		views.remove( view);
+	}
+
+} // end class HangmanModel
---
+++ GUIHangman/IHangmanView.java
@@ -0,0 +1,10 @@
+/**
+ * IHangmanView - view interface for MVC demo using Hangman
+ *
+ * @author David
+ * @version 1.00 2012/4/4
+ */
+public interface IHangmanView
+{
+	public void updateView( Hangman hangmanModel);
+}
